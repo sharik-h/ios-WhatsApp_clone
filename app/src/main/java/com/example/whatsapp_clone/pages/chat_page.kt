715d7700b5b0a635d65ba4ba73e1c5f1484fea23
Mohammed sharikh <mohammedsharik0123@gmail.com>
2022-02-05 11:50:56 +0530

    updated chat_page UI.

--- app/src/main/java/com/example/whatsapp_clone/pages/chat_page.kt
+++ app/src/main/java/com/example/whatsapp_clone/pages/chat_page.kt
@@ -1,54 +1,91 @@
 package com.example.whatsapp_clone.pages
 
+import android.graphics.drawable.Icon
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.whatsapp_clone.R
 
 @Composable
 fun chatPage() {
-    Box(Modifier.background(Color.Cyan)) {
+    Box() {
+
         Column(modifier = Modifier.fillMaxSize()) {
-            Text(text = "sample")
+
+            val bg: Painter = painterResource(id = R.drawable.wsappbg)
+            Image(
+                painter = bg,
+                contentDescription = "",
+                modifier = Modifier.fillMaxSize() )
         }
-        Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Bottom) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Bottom)
+        {
+            Divider(thickness = 1.dp, color = Color(0xFF686868))
             Row(modifier = Modifier
-                .background(Color.LightGray)
                 .height(60.dp)
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .background(Color.White),
                 horizontalArrangement = Arrangement.Center,
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Icon(imageVector = Icons.Default.Add, contentDescription = "", Modifier.size(30.dp))
 
+                val addimg: Painter = painterResource(id = R.drawable.plus__96)
+                Icon(
+                    painter = addimg,
+                    contentDescription = "",
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(30.dp)
+                )
 
+                val stickerimg: Painter = painterResource(id = R.drawable.icons8_sticker_96)
                 var msg by remember{ mutableStateOf("")}
                 OutlinedTextField(
                     value = "",
                     onValueChange = { newmsg -> msg = newmsg },
                     modifier = Modifier
-                        .background(Color.White)
+                        .background(Color(0xFFF6F6F6))
                         .height(40.dp)
-                        .width(260.dp)
+                        .width(230.dp)
                         .clip(RoundedCornerShape(50))
+                        .padding(end = 5.dp),
+                    trailingIcon = {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Image(painter = stickerimg, contentDescription = "select any sticker",
+                            modifier = Modifier.size(30.dp))
+                        }
+                    }
                 )
 
-                val camimg: Painter = painterResource(id = R.drawable.camera)
+                val rupeeimg: Painter = painterResource(id = R.drawable.icons8_rupee_96)
+                Image(
+                    painter = rupeeimg,
+                    contentDescription = "payment method icon",
+                    modifier = Modifier
+                        .padding(horizontal = 5.dp)
+                        .size(25.dp)
+                )
+
+                val camimg: Painter = painterResource(id = R.drawable.camera_96)
                 Image(
                     painter = camimg,
                     contentDescription = "cam image",
@@ -57,7 +94,7 @@ fun chatPage() {
                         .size(30.dp)
 
                 )
-                val micimg : Painter = painterResource(id = R.drawable.microphone)
+                val micimg : Painter = painterResource(id = R.drawable.microphone_96)
                 Image(
                     painter = micimg,
                     contentDescription = "cam image",
@@ -66,6 +103,65 @@ fun chatPage() {
                         .size(30.dp)
 
                 )
+            }
+        }
+
+        //AppBar
+        TopAppBar(
+            backgroundColor = Color.White,
+            modifier = Modifier
+                .fillMaxWidth()
+            ,
+        ) {
+            Row(modifier = Modifier.padding(vertical = 6.dp)) {
+                val backimg: Painter = painterResource(id = R.drawable.back_96)
+                IconButton(
+                    onClick = { /*TODO*/ },
+
+                ) {
+                    Icon(
+                        painter = backimg,
+                        contentDescription = "go back to listView page.",
+                    )
+                }
+                val profilepic: Painter = painterResource(id = R.drawable.img_0922)
+                Image(
+                    painter = profilepic,
+                    contentDescription = "",
+                    modifier = Modifier.clip(RoundedCornerShape(50))
+                )
+                Text(
+                    text = "Sharikh",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(top = 7.dp, start = 10.dp)
+                )
+                
+                Spacer(modifier = Modifier.width(110.dp))
+                
+                val videoimg: Painter = painterResource(id = R.drawable.video_camera_64)
+                IconButton(onClick = { /*TODO*/ }) {
+                    Image(
+                        painter = videoimg,
+                        contentDescription = "video call iconButton",
+                        modifier = Modifier
+                            .padding(horizontal = 5.dp)
+                            .size(30.dp)
+                    )
+                }
+                
+                Spacer(modifier = Modifier.width(10.dp))
+                
+                val phoneimg: Painter = painterResource(id = R.drawable.phone_50)
+                IconButton(onClick = { /*TODO*/ }) {
+                    Image(
+                        painter = phoneimg,
+                        contentDescription = "voice call iconButton",
+                        modifier = Modifier
+                            .padding(horizontal = 5.dp)
+                            .size(30.dp)
+                    )
+                }
             }
         }
     }
